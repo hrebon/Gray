67cd0dec990255b974b3c8331861aedf8aac0a80
hrebon <habibebon90@gmail.com>
2025-04-03 08:52:59 -0600

    new

---
+++ pages/services/index.js
@@ -0,0 +1,33 @@
+// components
+import ServiceSlider from '../../components/ServiceSlider'
+import BUlb from '../../components/Bulb'
+import Circles from '../../components/Circles'
+
+// framer motion
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
+
+
+const Services = () => {
+  return (
+    <div className='h-full bg-primary/30 py-36 flex items-center'>
+      <Circles></Circles>
+      <div className='container mx-auto'>
+        <div className='flex flex-col xl:flex-row gap-x-8' >
+          {/* text */}
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0' >
+            <motion.h2 variants={fadeIn('up', 0.2)} initial='hidden' animate='show' exit='hidden' className='h2 xl:mt-8'>My Services <span className='text-accent' >.</span> </motion.h2>
+            <motion.p variants={fadeIn('up', 0.2)} initial='hidden' animate='show' exit='hidden' className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Lorem isasdkfjadskf akdsjfakdsjfalds laksdfjalskd jlaskdjfalkdsjf ladkfjadskfjaklsdfjasdkljfalksdjfj dskfjasdlkjfadjfakdjf lajaskdfjadksjf akdsjf</motion.p>
+          </div>
+          {/* slider */}
+          <motion.div variants={fadeIn('down', 0.2)} initial='hidden' animate='show' exit='hidden' className='w-full xl:max-w-[65%]' >
+            <ServiceSlider />
+          </motion.div>
+        </div>
+      </div>
+      <BUlb></BUlb>
+    </div>
+  );
+};
+
+export default Services;
